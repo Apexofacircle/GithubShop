a87d0ee01d142202b8ee1edd18eb9db726c9c3d0
Ada <96547482+Apexofacircle@users.noreply.github.com>
2023-02-28 11:12:49 -0500

    update

--- script.js
+++ script.js
@@ -4,24 +4,26 @@ $("p").html("this")
 var _DNSJson = {}
 var is_Batman = true;
 var InfoToDisplay = "None";
+$.ajax({
+    method: "get",
+    url: "https://raw.githubusercontent.com/Apexofacircle/GitShopProduct1/main/GitShopInfo.json",
+    dataType: "json"
+  })
+    .done(function( msg ) {
+      console.log(msg)
+      _DNSJson = msg
+      console.log(_DNSJson.RepoInfo.Img)
+    });
+
 function batman(){
         
 
-        $.ajax({
-            method: "get",
-            url: "https://raw.githubusercontent.com/Apexofacircle/GitShopProduct1/main/GitShopInfo.json",
-            dataType: "json"
-          })
-            .done(function( msg ) {
-              console.log(msg)
-              _DNSJson = msg
-              console.log(_DNSJson.RepoInfo.Img)
-            });
+        
 
 
         var img = new Image();
-        img.src = "_DNSJson.RepoInfo.Img";
+        img.src = _DNSJson.RepoInfo.Img;
         document.body.append(img);
 }
 
-$("button").click(batman)
+$("Batman").click(batman)
