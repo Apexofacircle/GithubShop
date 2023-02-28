806a6c83348f207157c8d3e9a00b52cae595dba0
Ada <96547482+Apexofacircle@users.noreply.github.com>
2023-02-28 10:42:58 -0500

    update

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("It is working!")
+$("p").html("this")
+
+
+var _DNSJson = {}
+var is_Batman = true;
+var InfoToDisplay = "None";
+function batman(){
+        
+
+        $.ajax({
+            method: "get",
+            url: "https://raw.githubusercontent.com/Apexofacircle/GitShopProduct1/main/GitShopInfo.json",
+            dataType: "json"
+          })
+            .done(function( msg ) {
+              console.log(msg)
+              _DNSJson = msg
+              console.log(_DNSJson.RepoInfo.Img)
+            });
+
+
+        var img = new Image();
+        img.src = "https://raw.githubusercontent.com/Apexofacircle/GitShopProduct1/main/825d0083-a449-4dfe-a616-5bc6a99138a5.png";
+        document.body.append(img);
+}
+
+$("button").click(batman)
