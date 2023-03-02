21bab5342c00e34c03cb8df3a3cd3b7cefd2a2b5
Ada <96547482+Apexofacircle@users.noreply.github.com>
2023-03-02 15:20:59 -0500

    update

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var _DNSJson = {};
 
+function test(){
+    console.log("Test")
+}
+
 $.ajax({method: "get",url: "https://raw.githubusercontent.com/Apexofacircle/GitShopProduct1/main/GitShopInfo.json",dataType: "json"})
 .done(function( msg ) {
     _DNSJson = msg
@@ -13,20 +17,54 @@ $.ajax({method: "get",url: "https://raw.githubusercontent.com/Apexofacircle/GitS
 
         var _DNSSection = _DNSJson[i]
 
-
+        // generate variables from the Json file
         var _Image = _DNSSection["Img"]
         var _Name = _DNSSection["Name"]
         var _Description = _DNSSection["Description"]
-
+        var _Detailed_Description = _DNSSection["Detailed_Description"]
         console.log(_Image)
 
 
-        var _Card = ('<<div class="card"><div id="CardImage"><img src="'+_Image+'" alt="" class = "CardImg"></div><div class="card-body"><h5 class="card-title">'+_Name+'</h5><p class="card-text">'+_Description+'</p><a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample" ID="ExpandData">Link with href</a></div></div>');
+        var _Card = '<div class="card">' +
+                        '<div id="CardImage">' +
+                            '<img src="'+_Image+'" alt="" class = "CardImg">'+
+                        '</div>'+
+                        '<div class="card-body">'+
+                            '<h5 class="card-title">'+_Name+'</h5>'+
+                            '<p class="card-text">'+_Description+'</p>'+
+                            '<a href="#'+_Name+'"><button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">'+
+                            ' test '+
+                            '</button></a>'+
+                        '</div>'+
+                    '</div>';
         $("Cards").append(_Card);
     }
 
-    // Detailed info
-    $("#ExpandData").click(function test() { 
-        console.log("test work")
-    });
+    // Hold URL on load
+    let currentPage = location.href;
+
+    // listen for changes
+    setInterval(function()
+    {
+        if (currentPage != location.href)
+        {
+            //Gets the ID in the webpage
+            currentPage = location.href;
+            var _Href = window.location.href.split("#")[1]
+            
+            // Display title
+            var _OCTitle = '<h1>'+_Href+'</h1>'
+            $("OffCanvasTitle").html(_OCTitle)
+
+            //Display detailed information
+            var _DetailedInfo = _DNSJson[_Href]
+            console.log(_DetailedInfo)
+            var _OCText = '<P>'+_DetailedInfo["DetailedDescription"]+'</p>'
+            var _OCImage = '<img src="'+_DetailedInfo["Img"]+'" alt="" class = "OCImg">'
+
+            $("OffCanvasDetail").html(_OCText)
+            $("DisplayImage").html(_OCImage)
+        }
+    }, 500);
+    
 });
